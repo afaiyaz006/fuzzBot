43533f247a0626ed38afa4eddc3ccbd63851a2fc
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2021-06-11 18:40:07 +0600

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -31,5 +31,5 @@ async def on_message(message):
         random_message=motivational_quotes[random_index]    
         await message.channel.send(random_message)
 
-if __name__=="__main__":  
+if __name__=="__main__":
     client.run(TOKEN)
