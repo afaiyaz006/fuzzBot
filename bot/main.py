5bcc2d33d74156c394cc811f5e8d00476de6a3ab
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2021-06-12 22:08:54 +0600

    Revert "Revert "Update main.py""
    This reverts commit 2ca56c3efd52b5336d247243411fbd211b68a36e.

--- bot/main.py
+++ bot/main.py
@@ -35,16 +35,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if any(word in message.content.lower() for word in trigger_list):
+    elif any(word in message.content.lower() for word in trigger_list):
         random_index=random.randint(1,len(motivational_quotes)-1)
         random_message=motivational_quotes[random_index]    
         await message.channel.send(random_message)
 
-    if("hello" in message.content.lower()):
+    elif("hello" in message.content.lower()):
         await message.channel.send("Hello "+message.author.mention)
         await message.add_reaction(emoji)
     
-    if("surprise" in message.content.lower()):
+    elif("surprise" in message.content.lower()):
         #gifUrl=await t.arandom("surprise")
         mentions=[m.mention for m in message.mentions]
         if(len(mentions)>0):
@@ -53,7 +53,7 @@ async def on_message(message):
         else:
           await message.channel.send("surprise "+message.author.mention+"\n"+await getGif("surprise"))
 
-    if "aww" in  message.content.lower() or "cute" in message.content.lower() or "gol" in message.content.lower():
+    elif "aww" in  message.content.lower() or "cute" in message.content.lower() or "gol" in message.content.lower():
         await message.channel.send(await getGif("aww"))
         
 
