d847ad6fd3240c7f51f43046928e615e65113c90
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2021-06-12 18:11:21 +0600

    updated main.py

--- bot/main.py
+++ bot/main.py
@@ -38,11 +38,12 @@ async def on_message(message):
         random_index=random.randint(1,len(motivational_quotes)-1)
         random_message=motivational_quotes[random_index]    
         await message.channel.send(random_message)
+
     if("hello" in message.content.lower()):
         await message.channel.send("Hello "+message.author.mention)
         await message.add_reaction(emoji)
     
-    if("surprise" in message.content):
+    if("surprise" in message.content.lower()):
         #gifUrl=await t.arandom("surprise")
         mentions=[m.mention for m in message.mentions]
         if(len(mentions)>0):
@@ -52,6 +53,11 @@ async def on_message(message):
         else:
           await message.channel.send("surprise "+message.author.mention)
           await message.channel.send(await getGif("surprise"))
+          
+    if "aww" in  message.content.lower() or "cute" in message.content.lower():
+        await message.channel.send("surprise "+mention_list)
+        await message.channel.send(await getGif("aww"))
+        
 
 if __name__=="__main__":
     client.run(TOKEN)
